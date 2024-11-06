79358245bf3065283958788fa599903d27087aca
bayram celikarslan <bayram72@live.com>
2024-11-05 22:56:43 -0500

    fixed menu options and added texture to tutorial

--- Assets/Scripts/Main Menu/ResolutionManager.cs
+++ Assets/Scripts/Main Menu/ResolutionManager.cs
@@ -6,30 +6,41 @@ using System.Collections.Generic;
 public class ResolutionManager : MonoBehaviour
 {
     public TMP_Dropdown resolutionDropdown;
-    public Toggle fullscreenToggle;  // Add a reference for the fullscreen toggle
+    public Toggle fullscreenToggle;  // Reference for the fullscreen toggle
 
-    private Resolution[] resolutions;
+    private List<Resolution> uniqueResolutions;
 
     void Start()
     {
-        // Get available screen resolutions
-        resolutions = Screen.resolutions;
+        // Get all available screen resolutions
+        Resolution[] allResolutions = Screen.resolutions;
         resolutionDropdown.ClearOptions();
 
-        // Create a list to hold resolution options as strings
+        // Track unique resolutions by width and height
+        HashSet<string> uniqueResolutionStrings = new HashSet<string>();
+        uniqueResolutions = new List<Resolution>();
         List<string> options = new List<string>();
 
         int currentResolutionIndex = 0;
 
-        for (int i = 0; i < resolutions.Length; i++)
+        for (int i = 0; i < allResolutions.Length; i++)
         {
-            string option = resolutions[i].width + " x " + resolutions[i].height;
-            options.Add(option);
+            // Create a string to represent the width and height
+            string resolutionString = allResolutions[i].width + " x " + allResolutions[i].height;
 
-            if (resolutions[i].width == Screen.currentResolution.width &&
-                resolutions[i].height == Screen.currentResolution.height)
+            // Only add if it's not a duplicate width-height combination
+            if (uniqueResolutionStrings.Add(resolutionString))
             {
-                currentResolutionIndex = i;
+                // Add to unique resolutions list and dropdown options
+                uniqueResolutions.Add(allResolutions[i]);
+                options.Add(resolutionString);
+
+                // Check if this is the current resolution
+                if (allResolutions[i].width == Screen.currentResolution.width &&
+                    allResolutions[i].height == Screen.currentResolution.height)
+                {
+                    currentResolutionIndex = uniqueResolutions.Count - 1;
+                }
             }
         }
 
@@ -38,7 +49,7 @@ public class ResolutionManager : MonoBehaviour
         resolutionDropdown.RefreshShownValue();
 
         // Set initial fullscreen state
-        //fullscreenToggle.isOn = Screen.fullScreen;
+        fullscreenToggle.isOn = Screen.fullScreen;
 
         // Add listeners to handle resolution and fullscreen changes
         resolutionDropdown.onValueChanged.AddListener(SetResolution);
@@ -47,7 +58,7 @@ public class ResolutionManager : MonoBehaviour
 
     public void SetResolution(int resolutionIndex)
     {
-        Resolution resolution = resolutions[resolutionIndex];
+        Resolution resolution = uniqueResolutions[resolutionIndex];
         Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
     }
 
