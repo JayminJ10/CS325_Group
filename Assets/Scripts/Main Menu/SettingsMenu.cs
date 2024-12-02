92f5f9fb9d3fe0cfd711fd666944aa430f8d4f29
dogsonstage <167033448+dogsonstage@users.noreply.github.com>
2024-12-01 20:59:50 -0500

    volume slider function

--- Assets/Scripts/Main Menu/SettingsMenu.cs
+++ Assets/Scripts/Main Menu/SettingsMenu.cs
@@ -1,23 +1,24 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-  
+
 public class SettingsMenu : MonoBehaviour
 {
-    
-    /*public Slider volumeSlider;
-    public AudioSource gameAudioSource;
 
+    public Slider volumeSlider;
     void Start()
     {
         // Set the initial slider value to match the current audio volume
+        volumeSlider = GetComponent<Slider>();
         volumeSlider.value = AudioListener.volume;
     }
 
-    public void SetVolume()
+    public void SetVolume(float newValue)
     {
         // Adjust the global audio volume based on slider value
-        AudioListener.volume = volumeSlider.value;
+        float newVol = AudioListener.volume;
+        newVol = newValue;
+        AudioListener.volume = newVol;
     }
-    */
+
 }
